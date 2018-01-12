4c4dcffc569f6818101062664dabb5b1ba6d219f
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2018-01-12 16:49:36 +0100

    figaro installed

--- app/services/amazon_service.rb
+++ app/services/amazon_service.rb
@@ -2,9 +2,7 @@ require 'httparty'
 
 class AmazonService
   def self.call_api
-    full_challenge = HTTParty.get('https://e7lqr7l4z5.execute-api.eu-central-1.amazonaws.com/prod/NewCrewMemberChallenge', :headers => {"X-API-key" => "welcometothechallenge"})
+    full_challenge = HTTParty.get('https://e7lqr7l4z5.execute-api.eu-central-1.amazonaws.com/prod/NewCrewMemberChallenge', :headers => {"X-API-key" => ENV["AWS_API_KEY"]})
     coded_message = full_challenge.values.first
   end
 end
-
-
