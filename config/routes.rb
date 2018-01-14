e78cc5879f4710c4a398e82fe65abeb2e91105b2
Nicolas Jonckheere <nicolasjonckheere22@hotmail.com>
2018-01-14 18:12:11 +0100

    routing order

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'messages#index'
   get '/', to: 'messages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'messages#index'
 end
